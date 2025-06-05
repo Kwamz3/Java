43a0debf78d102979806ba2f63c483a983389250
Kwamz3 <charlie18appiah@gmail.com>
2025-06-05 12:53:41 +0000

    typeconversion

---
+++ typeconversion.java
@@ -0,0 +1,17 @@
+public class typeconversion {
+    public static void main(String a [])
+    {
+        // byte b = 127;
+        // int c = 257;
+        // byte b = (byte) c;
+
+        // float f = 5.6f;
+        // int t = (int) f;
+
+        byte b = 10;
+        byte c = 20;
+        int result = b * c;
+        
+        System.out.println(result);
+    }
+}
