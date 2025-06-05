83dc888b44219c79cb6261cf9b4f4709890caac0
Kwamz3 <charlie18appiah@gmail.com>
2025-06-05 12:13:13 +0000

    try commit

--- literals.java
+++ literals.java
@@ -1,14 +1,14 @@
 public class literals {
     public static void main(String a [])
     {
-        // int num1 = 0x7E;
-        // System.out.println(num1);
+        int num1 = 0x7E;
+        System.out.println(num1);
 
-        // long num2 = 10_000_000_00;
-        // System.out.println(num2);
+        long num2 = 10_000_000_00;
+        System.out.println(num2);
         
-        // double num3 = 12e11;
-        // System.out.println(num3);
+        double num3 = 12e11;
+        System.out.println(num3);
 
         char c = '2';
         c++;
