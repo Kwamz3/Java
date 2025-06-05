ba9e8985b154ddf2d361a747528a0686e2964403
Kwamz3 <charlie18appiah@gmail.com>
2025-06-05 12:10:31 +0000

    first commit

---
+++ literals.java
@@ -0,0 +1,17 @@
+public class literals {
+    public static void main(String a [])
+    {
+        // int num1 = 0x7E;
+        // System.out.println(num1);
+
+        // long num2 = 10_000_000_00;
+        // System.out.println(num2);
+        
+        // double num3 = 12e11;
+        // System.out.println(num3);
+
+        char c = '2';
+        c++;
+        System.out.println(c);
+    }
+}
---
+++ variables.java
@@ -0,0 +1,21 @@
+public class variables {
+    public static void main(String a [])
+    {
+        //int num1 = 9;
+        // byte by = 127;
+        // short sh = 558;
+        // long l = 585488l;
+        // float f = 5008.0000f;
+        // double d = 5008.00000;
+        // char c = 'k';
+        // boolean b = true;
+        
+        // System.out.println(l);
+        // System.out.println(f);
+        // System.out.println(d);
+        // System.out.println(sh); int num1 = 10;
+        // int num2 = 20;
+        // int result = num1 + num2;
+        // System.out.print(result);
+    }
+}
