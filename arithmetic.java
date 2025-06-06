10d96f33d7ff12baa49987cfda8c47701585f59b
Kwamz3 <charlie18appiah@gmail.com>
2025-06-06 21:03:01 +0000

    arithmeticJava

---
+++ arithmetic.java
@@ -0,0 +1,22 @@
+public class arithmetic {
+    public static void main(String args [])
+    {
+        // int num1 = 5;
+        int num2 = 7;
+
+        // int result = num2 - num1;
+        // int result = num2 * num1;
+        // int result = num2 / num1;
+        // int result = num2 % num1;
+        // num2 = num2 + 2;
+        // num2 += 2;
+        // num2 -= 2;
+        // num2 *= 8;
+        // num2++;
+        // num2--;
+
+        // int result = ++num2;
+        int result = num2++;
+        System.out.println(result);
+    }
+}
