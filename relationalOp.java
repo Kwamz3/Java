65261924371d4c76b46b55764bd3ff4873857af4
Kwamz3 <charlie18appiah@gmail.com>
2025-06-06 22:31:37 +0000

    relationalOp

---
+++ relationalOp.java
@@ -0,0 +1,10 @@
+public class relationalOp {
+    public static void main(String args[])
+    {
+        int x = 6;
+        int y = 6;
+        boolean result = x <= y;
+
+        System.out.println(result);
+    }
+}
