29917f5b827d631be585ad4ead1dac22eaf6f13c
Kwamz3 <charlie18appiah@gmail.com>
2025-06-08 18:12:23 +0000

    switchSta

--- switchSta.java
+++ switchSta.java
@@ -3,40 +3,84 @@ public class switchSta {
     {
         int n = 2;
 
-        switch (n) {
-            
+        // switch (n) {
+
+        //     case 1: {
+        //         System.out.println("Monday");
+        //     }
+        //         break;
+        //     case 2: {
+        //         System.out.println("Tuesday");
+        //     }
+        //         break;
+        //     case 3: {
+        //         System.out.println("Wednesday");
+        //     }
+        //         break;
+        //     case 4: {
+        //         System.out.println("Thursday");
+        //     }
+        //         break;
+        //     case 5: {
+        //         System.out.println("Friday");
+        //     }
+        //         break;
+        //     case 6: {
+        //         System.out.println("Saturday");
+        //     }
+        //         break;
+        //     case 7: {
+        //         System.out.println("Sunday");
+        //     }
+        //         break;
+        //     default: {
+        //         System.out.println("Please enter a valid number");
+        //     }
+        // }
+
+        
+        int a = 1;
+
+        switch(a){
+
             case 1: {
                 System.out.println("Monday");
             }
                 break;
+
             case 2: {
                 System.out.println("Tuesday");
             }
                 break;
+            
             case 3: {
                 System.out.println("Wednesday");
             }
                 break;
+
             case 4: {
                 System.out.println("Thursday");
             }
                 break;
+                
             case 5: {
                 System.out.println("Friday");
             }
                 break;
+
             case 6: {
                 System.out.println("Saturday");
             }
                 break;
+
             case 7: {
                 System.out.println("Sunday");
             }
                 break;
+
             default: {
-                System.out.println("Please enter a valid number");
+                System.out.println("Please enter a valid number!");
             }
         }
-
     }
 }
