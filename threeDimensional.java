d60844dc52f0c9f19e22b39632bda58dea683d8a
Kwamz3 <charlie18appiah@gmail.com>
2025-06-21 13:39:32 +0000

    threeDimensional

---
+++ threeDimensional.java
@@ -0,0 +1,28 @@
+public class threeDimensional {
+    public static void main(String args [])
+    {
+        int nums[][][] = new int[2][3][2];
+        // nums[0] = new int[3];
+        // nums[1] = new int[4];
+        // nums[2] = new int[1];
+        // nums[2][0] = new int[1];
+        // nums[2][1] = new int[5];
+        // nums[2][2] = new int[7];
+        // nums[2][3] = new int[4];
+
+        for (int i = 0; i < nums.length; i++) {
+
+            for (int j = 0; j < nums[i].length; j++) {
+
+                for (int k = 0; k < nums[i][j].length; k++) {
+                    System.out.print(nums[i][j][k] = (int) (Math.random() * 10));
+                    System.out.print(nums[i][j][k] + " ");
+                }
+
+                System.out.println();
+            }
+
+            System.out.println();
+        }
+    }
+}
